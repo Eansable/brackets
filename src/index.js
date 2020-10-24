8e7955105e81b506cd35f8cea8c157cd7682f14b
Andrei Tarasevich <eansable@yandex.by>
2020-10-24 07:54:02 +0300

    feat: relized function check with 2 test failed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let chars = str.split(''),
+        stack = [],
+        open = [],
+        close = [],
+        equals = [],
+        closeIndex,
+        equalsIndex,
+        countEqualsBrackets = 0,
+        openIndex;
+
+  for (let i = 0; i < bracketsConfig.length; i++){
+    if (bracketsConfig[i][1] !== bracketsConfig[i][0]) {
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);}
+    else {equals.push(bracketsConfig[i][1]);}
+  }
+
+        for (let i = 0; i < chars.length; i++) {
+          equalsIndex = equals.indexOf(chars[i])
+           if (equalsIndex !== -1) {
+              countEqualsBrackets++;
+              if (countEqualsBrackets % 2 !== 0){
+                stack.push(chars[i]);
+              continue;}
+              else {
+                if (chars[i] !== stack.pop()) {
+                  return false;
+                }
+              }
+           }
+          openIndex = open.indexOf(chars[i]);
+          if (openIndex !== -1) {
+              stack.push(openIndex);
+
+              continue;
+          }
+          closeIndex = close.indexOf(chars[i]);
+       if (closeIndex !== -1) {
+           openIndex = stack.pop();
+           if (closeIndex !== openIndex) {
+               return false;
+           }
+          }
+        }
+           if (stack.length !== 0) {
+            return false;
+
+
+    }
+        return true;
 }
+
