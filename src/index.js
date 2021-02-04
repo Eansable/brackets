e23094f434628dddd61d1d29ffdd1005a801dc55
Andrei Tarasevich <eansable@yandex.by>
2021-02-04 22:01:10 +0300

    fix: fixed problems with equals chairs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check1(str, bracketsConfig) {
   let chars = str.split(''),
         stack = [],
         open = [],
@@ -51,3 +51,42 @@ module.exports = function check(str, bracketsConfig) {
         return true;
 }
 
+module.exports = function check(str, bracketsConfig) {
+
+  let chars = str.split('');
+  const open = [];
+  const close = [];
+  const stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    open.push(bracketsConfig[i][0]);
+    close.push(bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < chars.length; i++) {
+    if (stack.length === 0) {
+    if (open.indexOf(chars[i]) >= 0) {
+      stack.push(chars[i]);
+    }
+    else {
+      if (open.indexOf(stack[stack.length - 1]) === close.indexOf(chars[i])) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  } else {
+    if (close.indexOf(chars[i]) >= 0){
+        if (open.indexOf(stack[stack.length - 1]) === close.indexOf(chars[i])) {
+        stack.pop();
+      } else if (close.indexOf(chars[i]) === open.indexOf(chars[i])) {stack.push(chars[i])}
+      else  {
+        return false;
+      }
+    } else {
+      stack.push(chars[i]);
+    }
+  }
+}
+  return stack.length === 0 ? true : false;
+}
